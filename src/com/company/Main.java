8c4e56aa96948a3f3e1e250b5ecdc27b204cd271
Luc modolo <l.modolo@rafisa.ch>
2021-09-28 07:52:00 +0200

    Das ist die neue Car Funktion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
 
-/*
-        int tmp = MathHelper.min(423, 345);
-
-        System.out.println(tmp);
-
-        double tmp2 = MathHelper.min(5.5,6.5);
-
-        System.out.println(tmp2);
-*/
         car c1 = new car("BMW",100);
         c1.drive();
 
-        car c2 = new car("Fiat");
+      /*  car c2 = new car("Fiat");
         c2.drive();
 
-        car c4 = new car("Fiat", hp: 200, kg: 300);
-        c4.drive();
+        car c3 = new car("Rolls",250,2500);
+
+        c3.drive();
+      */
 
 
     }
--- src/com/company/car.java
+++ src/com/company/car.java
@@ -2,20 +2,34 @@ package com.company;
 
     public class car {
         private String brand;
-        private int hp,kg;
+        private int hp;
+        private double wigth;
 
         car(String brand, int hp){
             this.brand = brand;
             this.hp = hp;
         }
 
-        car(String brand, int hp, int kg){
+        car(String brand){
+            this.brand = brand;
+        }
+         car(String brand,int hp,double wigth){
             this.brand = brand;
             this.hp = hp;
-            this.kg = kg;
-        }
+            this.wigth = wigth;
+         }
+
 
         public void drive(){
-            System.out.println("Ein "+brand+" fährt mit "+hp+"PS und hat "+kg+" gewicht");
+            if (wigth == 2600) {
+                System.out.println("Der "+brand+" hat "+hp+" PS und wiegt  "+ wigth+ " kg");
+            }
+            if (hp != 0){
+                System.out.println("Der "+brand);
+            }
+            else if (wigth != 0){
+                System.out.println("Der "+brand+" hat "+hp+" PS");
+            }
+            System.out.println("Der "+brand+" hat "+hp+" und hat ein Gewicht von "+wigth);
         }
 }
